446f90abd4d7ab5a5e970847a00b764056329158
George Mathias <gmathias80@gmail.com>
2021-11-27 14:34:35 -0300

    Configurando o projeto e criando o primeiro teste Healthcheck

---
+++ cypress/integration/ping.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+import req from '../support/api/requests'
+import assertions from '../support/api/assertions'
+
+context('Ping', () => {
+    it('GET HealthCheck', () => {
+        req.getPing().then(getPingResponse => {
+            assertions.shouldHaveStatus(getPingResponse, 200)
+        })
+    });
+});
